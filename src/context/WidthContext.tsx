8729a1d39e7fd58e2cfb96c7772397548b70d57f
TranDanh1122 <trandanh14042000@gmail.com>
2025-01-24 20:20:57 +0700

    fix bug

--- src/context/WidthContext.tsx
+++ src/context/WidthContext.tsx
@@ -3,7 +3,8 @@ import React from "react";
 //chỗ này đáng ra là  nằm trong theem luôn mới hợp lí, mà ban đầu không tính tới
 export const WidthContext = React.createContext<{ width: number, setWidth: React.Dispatch<React.SetStateAction<number>> }>({ width: window.innerWidth, setWidth: () => { } })
 export default function WidthContextProvider({ children }: { children: React.ReactNode }): React.JSX.Element {
-    const [width, setWidth] = React.useState<number>(window.innerHeight)
+    const [width, setWidth] = React.useState<number>(window.innerWidth)
+    
     const timer = React.useRef<number | null>(null)
     React.useEffect(() => {
         const handleResize = () => {
